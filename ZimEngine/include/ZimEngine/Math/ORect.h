7705ef22dccfc72396ac42895e90ed6a2177b584
Zim <98758788+hazimbharis@users.noreply.github.com>
2025-02-11 07:50:24 +0800

    added an ORect class for rendering basic geometry

---
+++ ZimEngine/include/ZimEngine/Math/ORect.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <ZimEngine/OPrerequisites.h>
+
+class ORect
+{
+public:
+    ORect()
+    {
+        
+    }
+
+public:
+    int width = 0, height = 0, left = 0, top = 0;
+};
--- ZimEngine/include/ZimEngine/OPrerequisites.h
+++ ZimEngine/include/ZimEngine/OPrerequisites.h
@@ -1,3 +1,4 @@
 #pragma once
 
 typedef float f32;
+typedef int i32;
